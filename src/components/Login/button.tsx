346b2e7e7d396fc81a81e543627726f699885bcd
kimzini <rlawlsdl11111@gmail.com>
2024-10-14 20:12:48 +0900

    style: 버튼컴포넌트 텍스트 수정

--- src/components/Login/button.tsx
+++ src/components/Login/button.tsx
@@ -10,8 +10,8 @@ interface ButtonProps {
 export const ConfirmButton: React.FC<ButtonProps> = ({ isChecked, onClick, disabled, children }) => {
   return (
     <button
-      className={`flex items-center justify-center px-[7.5rem] py-[0.88rem] rounded-[0.3125rem] ${
-        isChecked ? 'bg-primary text-white' : 'bg-grayscale-20 text-grayscale-60'
+      className={`flex items-center justify-center px-[7.5rem] py-[0.88rem] rounded-[0.3125rem] body1-medium ${
+        isChecked ? 'bg-primary text-grayscale-90' : 'bg-grayscale-20 text-grayscale-60'
       }`}
       disabled={disabled ?? !isChecked}
       onClick={onClick}
