3206ac8e72d7606181e3dfd7f91df73da880c29a
kimzini <rlawlsdl11111@gmail.com>
2024-10-14 14:27:43 +0900

    style: 버튼 컴포넌트 추가

---
+++ src/components/Login/button.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface ButtonProps {
+  isChecked: boolean;
+  onClick?: () => void;
+  disabled?: boolean;
+  children: React.ReactNode;
+}
+
+export const ConfirmButton: React.FC<ButtonProps> = ({ isChecked, onClick, disabled, children }) => {
+  return (
+    <button
+      className={`flex items-center justify-center px-[7.5rem] py-[0.88rem] rounded-[0.3125rem] ${
+        isChecked ? 'bg-primary text-white' : 'bg-grayscale-20 text-grayscale-60'
+      }`}
+      disabled={disabled ?? !isChecked}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
