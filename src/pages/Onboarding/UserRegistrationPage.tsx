8c5d292c1b9c03cf74bbba84ee02158467c87117
kimzini <rlawlsdl11111@gmail.com>
2024-10-16 02:36:14 +0900

    feat: 취향 등록 완료 이동 경로 설정

--- src/pages/Onboarding/UserRegistrationPage.tsx
+++ src/pages/Onboarding/UserRegistrationPage.tsx
@@ -4,6 +4,8 @@ import { SelectProfile } from "./components/SelectProfile";
 import { SelectPrice } from "./components/SelectPrice";
 import { SelectLocation } from "./components/SelectLocation";
 import { SelectConcept } from "./components/SelectConcept";
+import { SelectFeature } from "./components/SelectFeature";
+import { useNavigate } from "react-router-dom";
 
 export const UserRegistrationPage = () => {
   const [progressValue, setProgressValue] = useState<number>(20);
@@ -11,6 +13,12 @@ export const UserRegistrationPage = () => {
   const handleNextStep = () => {
     setProgressValue((prevValue) => Math.min(prevValue + 20, 100));
   };
+
+  const navigate = useNavigate();
+
+  const handleConfirmClick = () => {
+      navigate("/create/complete");
+  }
 
   const renderComponent = () => {
     switch (progressValue) {
@@ -22,6 +30,8 @@ export const UserRegistrationPage = () => {
         return <SelectLocation onConfirm={handleNextStep} />;
       case 80:
         return <SelectConcept onConfirm={handleNextStep} />;
+      case 100:
+        return <SelectFeature onConfirm={handleConfirmClick} />
       default:
         return null;
     }
