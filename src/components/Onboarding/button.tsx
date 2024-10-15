16c8ac441a6c3b0ef24f5654209b1443a84b77e4
kimzini <rlawlsdl11111@gmail.com>
2024-10-15 22:21:58 +0900

    style: 버튼 컴포넌트 구현

---
+++ src/components/Onboarding/button.tsx
@@ -0,0 +1,31 @@
+interface ButtonProps {
+    isChecked: boolean;
+    onClick?: () => void;
+    children: React.ReactNode;
+  }
+
+export const TypeButton: React.FC<ButtonProps> = ({ isChecked, onClick, children }) => {
+    return(
+        <button
+        className={`min-w-[10rem] items-center justify-center py-[0.88rem] rounded-[0.31rem] body1-medium ${
+          isChecked ? 'bg-grayscale-80 text-background-dark' : 'bg-grayscale-30 text-grayscale-80'
+        }`}
+        onClick={onClick}
+        >
+        {children}
+      </button>
+    );
+};
+
+export const ConceptButton: React.FC<ButtonProps> = ({ isChecked, onClick, children }) => {
+    return(
+        <button
+        className={`items-center justify-center px-[3rem] py-[3rem] rounded-[0.31rem] body1-medium ${
+          isChecked ? 'bg-grayscale-80 text-background-dark' : 'bg-grayscale-30 text-grayscale-80'
+        }`}
+        onClick={onClick}
+        >
+        {children}
+      </button>
+    );
+};
