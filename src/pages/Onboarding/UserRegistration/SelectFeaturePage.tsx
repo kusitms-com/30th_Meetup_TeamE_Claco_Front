5df3670bbad83bf049c9ddb8b46b3fc5698f54f8
kimzini <rlawlsdl11111@gmail.com>
2024-10-17 18:49:20 +0900

    feat: 공연 특징 선택 페이지 퍼블리싱

---
+++ src/pages/Onboarding/UserRegistration/SelectFeaturePage.tsx
@@ -0,0 +1,143 @@
+import { ConfirmButton } from "@/components/Login/button";
+import { ConceptButton } from "@/components/Onboarding/button";
+import { Progress } from "@/components/ui/progress";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+interface Feature {
+  title: string;
+  description: string[];
+}
+
+export const SelectFeaturePage = () => {
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [selectedFeature, setSelectedFeature] = useState<string | null>(null);
+  const [selectedAllFeature, setSelectedAllFeature] = useState<string[]>([]);
+
+  const features: Feature[] = [
+    {
+      title: "웅장한",
+      description: [
+        "큰 규모의 오케스트라나",
+        "무대 장치, 강렬한 감정이 느껴지는 공연",
+      ],
+    },
+    {
+      title: "섬세한",
+      description: [
+        "작은 규모의 연주나 무대",
+        "미세한 감정의 변화와 정교함이 돋보이는 공연",
+      ],
+    },
+    {
+      title: "고전적인",
+      description: [
+        "고전적인 형식과 규칙을 따르는 클래식 공연",
+        "예) 고전 교향곡",
+      ],
+    },
+    {
+      title: "현대적인",
+      description: ["혁신적이고 새로운 형식의 공연", "예) 현대 무용, 클래식"],
+    },
+    {
+      title: "서정적인",
+      description: ["감정적으로 부드럽고 서정적인 음악과 무대"],
+    },
+    {
+      title: "역동적인",
+      description: ["강한 움직임과 템포가 특징인", "빠르고 에너지 넘치는 공연"],
+    },
+    {
+      title: "낭만적인",
+      description: [
+        "따뜻하고 감미로운 분위기로",
+        "사랑과 감성을 주제로 한 공연",
+      ],
+    },
+    {
+      title: "비극적인",
+      description: ["슬프고 어두운 감정을 전달하는 공연"],
+    },
+    {
+      title: "친숙한",
+      description: [
+        "대중들에게 친숙한 곡이나 춤을 기반으로 한 공연",
+        "예) 지브리 OST 공연, 비발디 ‘사계’ ",
+      ],
+    },
+    {
+      title: "새로운",
+      description: ["평소 자주 들어보지 못했던 새로운 곡을 기반으로 한 공연"],
+    },
+  ];
+
+  const navigate = useNavigate();
+
+  const handleFeatureClick = (feature: string) => {
+    setSelectedFeature(feature); 
+  };
+
+  const handleNextClick = () => {
+    if (selectedFeature) {
+      setSelectedAllFeature((prevFeatures) => [...prevFeatures, selectedFeature]);
+      setSelectedFeature(null);
+
+      if (currentStep >= features.length - 2) {
+        navigate("/create/complete");
+      } else {
+        setCurrentStep((prevStep) => prevStep + 2);
+      }
+    }
+  };
+
+  return (
+    <div className="w-full h-screen bg-background-dark">
+      <div className="flex flex-col w-full h-full px-[1.25rem] pt-[4.45rem] pb-[4rem] gap-[0.75rem]">
+        <div className="flex flex-col gap-[2.12rem]">
+          <Progress value={100} />
+          <span className="heading1-bold text-grayscale-90">
+            울랄라님의 취향에 맞는
+            <br />
+            클래식 공연을 알아볼까요?
+          </span>
+        </div>
+
+        <div className="flex flex-col justify-between h-full">
+          <div className="flex flex-col gap-[2.87rem]">
+            <span className="body1-medium text-grayscale-60">
+              선호하는 공연의 특징을 선택해주세요.
+            </span>
+            <div className="flex flex-col gap-[0.75rem]">
+              {features.slice(currentStep, currentStep + 2).map((feature) => (
+                <ConceptButton
+                  key={feature.title}
+                  isChecked={selectedFeature === feature.title}
+                  onClick={() => handleFeatureClick(feature.title)}
+                >
+                  <span className="heading1-bold mb-[0.62rem]">{feature.title}</span>
+                  <br />
+                  <span className="body3-normal">
+                    {feature.description.map((line, index) => (
+                      <span key={index}>
+                        {line}
+                        <br />
+                      </span>
+                    ))}
+                  </span>
+                </ConceptButton>
+              ))}
+            </div>
+          </div>
+
+          <ConfirmButton
+            isChecked={!!selectedFeature}
+            onClick={handleNextClick}
+          >
+          다음
+          </ConfirmButton>
+        </div>
+      </div>
+    </div>
+  );
+};
