7fb2b81556345647e9e5c80ed6760588430ba9b6
dvp-tae <th3016@naver.com>
2024-10-17 18:10:36 +0900

    feat: 푸터 활성화 상태 표시

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -2,30 +2,88 @@ import Home from "@/assets/svgs/home.svg?react";
 import Search from "@/assets/svgs/search.svg?react";
 import Ticket from "@/assets/svgs/ticket.svg?react";
 import User from "@/assets/svgs/user.svg?react";
+import { cn } from "@/lib/utils";
+import { useState } from "react";
+
+type FooterState = {
+  home: boolean;
+  search: boolean;
+  ticket: boolean;
+  user: boolean;
+};
 
 const Footer = () => {
+  const [isActive, setIsActive] = useState<FooterState>({
+    home: true,
+    search: false,
+    ticket: false,
+    user: false,
+  });
+
+  const handleClick = (tab: keyof FooterState) => {
+    setIsActive({
+      home: false,
+      search: false,
+      ticket: false,
+      user: false,
+      [tab]: true,
+    });
+  };
+
   return (
     <footer className="h-[74px] w-screen max-w-[450px] text-common-white bg-[#202020] shadow-footer fixed bottom-0 z-50">
       <div className="flex justify-around space-x-5 px-5 py-[10px]">
-        <div className="flex-col items-center justify-center w-12 h-6 text-center">
+        <div
+          className={
+            (cn(
+              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
+            ),
+            isActive.home ? `text-primary-500` : ``)
+          }
+          onClick={() => handleClick("home")}
+        >
           <span className="flex justify-center">
             <Home />
           </span>
           <span className="text-[11px] text-bold leading-none">추천</span>
         </div>
-        <div className="flex-col items-center justify-center w-12 h-6 text-center">
-          <span className="flex justify-center">
+        <div
+          className={
+            (cn(
+              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
+            ),
+            isActive.search ? `text-primary-500` : ``)
+          }
+          onClick={() => handleClick("search")}
+        >
+          <span className="flex justify-center ">
             <Search />
           </span>
           <span className="text-[11px] text-bold leading-none">둘러보기</span>
         </div>
-        <div className="flex-col items-center justify-center w-12 h-6 text-center">
+        <div
+          className={
+            (cn(
+              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
+            ),
+            isActive.ticket ? `text-primary-500` : ``)
+          }
+          onClick={() => handleClick("ticket")}
+        >
           <span className="flex justify-center">
             <Ticket />
           </span>
           <span className="text-[11px] text-bold leading-none">티켓</span>
         </div>
-        <div className="flex-col items-center justify-center w-12 h-6 text-center">
+        <div
+          className={
+            (cn(
+              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
+            ),
+            isActive.user ? `text-primary-500` : ``)
+          }
+          onClick={() => handleClick("user")}
+        >
           <span className="flex justify-center">
             <User />
           </span>
