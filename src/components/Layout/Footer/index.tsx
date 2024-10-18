fa982aac5ee427cf2d8250d8482cee4006c22a79
dvp-tae <th3016@naver.com>
2024-10-18 14:29:27 +0900

    style: 푸터 활성화 폰트 수정

--- src/components/Layout/Footer/index.tsx
+++ src/components/Layout/Footer/index.tsx
@@ -2,7 +2,6 @@ import Home from "@/assets/svgs/home.svg?react";
 import Search from "@/assets/svgs/search.svg?react";
 import Ticket from "@/assets/svgs/ticket.svg?react";
 import User from "@/assets/svgs/user.svg?react";
-import { cn } from "@/lib/utils";
 import { useState } from "react";
 
 type FooterState = {
@@ -34,60 +33,56 @@ const Footer = () => {
     <footer className="h-[74px] w-screen max-w-[450px] text-common-white bg-[#202020] shadow-footer fixed bottom-0 z-50">
       <div className="flex justify-around space-x-5 px-5 py-[10px]">
         <div
-          className={
-            (cn(
-              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
-            ),
-            isActive.home ? `text-primary-500` : ``)
-          }
+          className={`flex-col items-center justify-center w-12 h-6 text-center cursor-pointer ${isActive.home ? "text-primary-500" : ""}`}
           onClick={() => handleClick("home")}
         >
           <span className="flex justify-center">
             <Home />
           </span>
-          <span className="text-[11px] text-bold leading-none">추천</span>
+          <span
+            className={`text-[11px] ${isActive.home ? "font-bold" : "font-medium"} leading-none`}
+          >
+            추천
+          </span>
         </div>
         <div
-          className={
-            (cn(
-              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
-            ),
-            isActive.search ? `text-primary-500` : ``)
-          }
+          className={`flex-col items-center justify-center w-12 h-6 text-center cursor-pointer ${isActive.search ? "text-primary-500" : ""}`}
           onClick={() => handleClick("search")}
         >
           <span className="flex justify-center ">
             <Search />
           </span>
-          <span className="text-[11px] text-bold leading-none">둘러보기</span>
+          <span
+            className={`text-[11px] ${isActive.search ? "font-bold" : "font-medium"} leading-none`}
+          >
+            둘러보기
+          </span>
         </div>
         <div
-          className={
-            (cn(
-              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
-            ),
-            isActive.ticket ? `text-primary-500` : ``)
-          }
+          className={`flex-col items-center justify-center w-12 h-6 text-center cursor-pointer ${isActive.ticket ? "text-primary-500" : ""}`}
           onClick={() => handleClick("ticket")}
         >
           <span className="flex justify-center">
             <Ticket />
           </span>
-          <span className="text-[11px] text-bold leading-none">티켓</span>
+          <span
+            className={`text-[11px] ${isActive.ticket ? "font-bold" : "font-medium"} leading-none`}
+          >
+            티켓
+          </span>
         </div>
         <div
-          className={
-            (cn(
-              "flex-col items-center justify-center w-12 h-6 text-center cursor-pointer "
-            ),
-            isActive.user ? `text-primary-500` : ``)
-          }
+          className={`flex-col items-center justify-center w-12 h-6 text-center cursor-pointer ${isActive.user ? "text-primary-500" : ""}`}
           onClick={() => handleClick("user")}
         >
           <span className="flex justify-center">
             <User />
           </span>
-          <span className="text-[11px] text-bold leading-none">마이페이지</span>
+          <span
+            className={`text-[11px] ${isActive.user ? "font-bold" : "font-medium"} leading-none`}
+          >
+            마이페이지
+          </span>
         </div>
       </div>
     </footer>
