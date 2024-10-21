eade141ae0a2227c2edfb46e11326e8286007e12
kimzini <rlawlsdl11111@gmail.com>
2024-10-21 21:38:20 +0900

    feat: 기존 Button props 상속

--- src/components/Onboarding/Registration/index.tsx
+++ src/components/Onboarding/Registration/index.tsx
@@ -1,43 +1,41 @@
-interface ButtonProps {
-    isChecked: boolean;
-    onClick?: () => void;
-    children: React.ReactNode;
-  }
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isChecked: boolean;
+}
 
-export const TypeButton: React.FC<ButtonProps> = ({ isChecked, onClick, children }) => {
-    return(
-        <button
-        className={`h-screen min-w-[8rem] max-h-[3.5rem] items-center justify-center rounded-[0.31rem] body1-medium ${
-          isChecked ? 'bg-grayscale-80 text-dark' : 'bg-grayscale-30 text-grayscale-80'
-        }`}
-        onClick={onClick}
-        >
-        {children}
-      </button>
-    );
+export const TypeButton = ({ isChecked, children, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`h-screen min-w-[8rem] max-h-[3.5rem] items-center justify-center rounded-[0.31rem] body1-medium ${
+        isChecked ? 'bg-grayscale-80 text-dark' : 'bg-grayscale-30 text-grayscale-80'
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
 
-export const ConceptButton: React.FC<ButtonProps> = ({ isChecked, onClick, children }) => {
-    return(
-        <button
-        className={`items-center justify-center py-[3rem] rounded-[0.31rem] body1-medium ${
-          isChecked ? 'bg-grayscale-80 text-dark' : 'bg-grayscale-30 text-grayscale-80'
-        }`}
-        onClick={onClick}
-        >
-        {children}
-      </button>
-    );
+export const ConceptButton = ({ isChecked, children, onClick }: ButtonProps) => {
+  return (
+    <button
+      className={`items-center justify-center py-[3rem] rounded-[0.31rem] body1-medium ${
+        isChecked ? 'bg-grayscale-80 text-dark' : 'bg-grayscale-30 text-grayscale-80'
+      }`}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 };
 
-export const FeatureButton: React.FC<ButtonProps> = ({ isChecked, onClick, children }) => {
-  return(
-      <button
+export const FeatureButton = ({ isChecked, children, onClick }: ButtonProps) => {
+  return (
+    <button
       className={`h-screen items-center justify-center max-h-[9rem] py-[1rem] rounded-[0.31rem] body1-medium ${
         isChecked ? 'bg-grayscale-80 text-dark' : 'bg-grayscale-30 text-grayscale-80'
       }`}
       onClick={onClick}
-      >
+    >
       {children}
     </button>
   );
--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -1,20 +1,17 @@
-interface ButtonProps {
-    isChecked: boolean;
-    onClick?: () => void;
-    disabled?: boolean;
-    children: React.ReactNode;
-  }
-  
-  export const ConfirmButton: React.FC<ButtonProps> = ({ isChecked, onClick, disabled, children }) => {
-    return (
-      <button
-        className={`flex items-center justify-center px-[7.5rem] py-[0.88rem] rounded-[0.3125rem] body1-medium ${
-          isChecked ? 'bg-primary text-grayscale-90' : 'bg-grayscale-20 text-grayscale-60'
-        }`}
-        disabled={disabled ?? !isChecked}
-        onClick={onClick}
-      >
-        {children}
-      </button>
-    );
-  };
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isChecked: boolean;
+}
+
+export const ConfirmButton = ({ isChecked, onClick, disabled, children }: ButtonProps) => {
+  return (
+    <button
+      className={`flex items-center justify-center px-[7.5rem] py-[0.88rem] rounded-[0.3125rem] body1-medium ${
+        isChecked ? 'bg-primary text-grayscale-90' : 'bg-grayscale-20 text-grayscale-60'
+      }`}
+      disabled={disabled ?? !isChecked}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
