3c16c11387eefd4499371f057d411852f3540b19
dvp-tae <th3016@naver.com>
2024-11-06 18:45:30 +0900

    "remove: main containers file"

--- src/pages/Main/containers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as NearbyEventsSection } from "./NearbyEventsSection";
-export { default as PersonalizedSection } from "./PersonalizedSection";
-export { default as SimilarCollectionSection } from "./SimilarCollectionSection";
-export { default as SimilarShowsSection } from "./SimilarShowsSection";
