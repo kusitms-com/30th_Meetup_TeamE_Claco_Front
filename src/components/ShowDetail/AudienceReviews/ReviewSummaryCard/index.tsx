96456325605a0e8922df2086db2b95d6b69af0ef
kimzini <rlawlsdl11111@gmail.com>
2024-11-23 19:25:08 +0900

    style: 리뷰 요약 카드 높이 수정

--- src/components/ShowDetail/AudienceReviews/ReviewSummaryCard/index.tsx
+++ src/components/ShowDetail/AudienceReviews/ReviewSummaryCard/index.tsx
@@ -33,7 +33,7 @@ export const ReviewSummaryCard = ({
         </div>
       </div>
 
-      <span className="max-h-[50px] line-clamp-2 body2-semibold text-white tracking-[-0.28px]">
+      <span className="h-[40px] line-clamp-2 body2-semibold text-white tracking-[-0.28px]">
         {content}
       </span>
     </div>
