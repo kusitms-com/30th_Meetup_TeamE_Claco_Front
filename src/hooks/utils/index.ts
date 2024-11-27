b74ade4daa067d05b55d5390ebeeeaf2b9378e60
dvp-tae <th3016@naver.com>
2024-11-28 01:50:06 +0900

    feat: 모바일 뷰에서 날짜 정보 오류 이슈 해결

--- src/hooks/utils/index.ts
+++ src/hooks/utils/index.ts
@@ -2,7 +2,7 @@ export { default as useTruncateText } from "./useTruncateText";
 export { default as useThumbnailModal } from "./useThumbnailModal";
 export { default as useDebouncedState } from "./useDebouncedState";
 export { default as useShowFilter } from "./useShowFilter";
-export { default as formatDate } from "./formatYYYYMMDD";
+export { default as formatDateYYYYMMDD } from "./formatYYYYMMDD";
 export { default as extractDateRange } from "./extractDateRange";
 export { default as extractPricesWithSeats } from "./extractPricesWithSeats";
 export { default as extractSchedule } from "./extractSchedule";
